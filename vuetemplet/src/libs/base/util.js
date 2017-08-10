b12a4d86864294f981e788a869c79e8fda1cfa2a
wangding <1024564775@qq.com>
2017-08-10 18:40:57 +0800

    增加排序算法
    todo：待测试
    question：待测试

--- vuetemplet/src/libs/base/util.js
+++ vuetemplet/src/libs/base/util.js
@@ -4,6 +4,49 @@
  */
 const util = {
   /**
+   * Created by wang.ding on 2017/8/10.
+   * 工具类
+   * 数组根据其中某个值进行冒泡排序
+   * @param {object} dataList 数组
+   * @param {object} k 排序基准值
+   * @param {object} desc 升序还是降序
+   * @return {undefined || array} 数组的值或者为空
+   */
+  arraySortByTag(dataList, k, desc) {
+      if (typeof  dataList !== 'object') {
+          return;
+      }
+      var bubbleSort = function (a, b) {
+          if (a === undefined) {
+              return false;
+          } else if (b === undefined) {
+              return true;
+          }
+          return a > b;
+      };
+      if (k === undefined || k === '') {
+          return;
+      }
+      for (let i = 0; i < dataList.length; i++) {
+          for (let j = 0; i + j < dataList.length - 1; j++) {
+              if (desc === false) {
+                  if (bubbleSort(dataList[j][k], dataList[j + 1][k])) {
+                      const dataCk = dataList[j];
+                      dataList[j] = dataList[j + 1];
+                      dataList[j + 1] = dataCk;
+                  }
+              } else {
+                  if (bubbleSort(dataList[j + 1][k], dataList[j][k])) {
+                    const dataCk = dataList[j];
+                    dataList[j] = dataList[j + 1];
+                    dataList[j + 1] = dataCk;
+                  }
+              }
+          }
+      }
+      return dataList;
+  },
+  /**
    * Created by wang.ding on 2017/8/7.
    * 工具类
    * 根据值移除数组中的某个元素（仅仅针对非复杂对象的数组）
@@ -12,16 +55,18 @@ const util = {
    * @return {array} data 数组的值
    */
   arrRemoveByValue(dataList, value) {
-    if (dataList.length) {
-      for (let i = 0; i < dataList.length; i++) {
-        if (dataList[i] === value) {
-          dataList[i] = dataList[i + 1];
-        }
+      if (dataList.length) {
+          for (let i = 0; i < dataList.length; i++) {
+              if (dataList[i] === value) {
+                  dataList[i] = dataList[i + 1];
+              }
+          }
+          dataList.length--;
+          return dataList;
       }
-      dataList.length--;
-      return dataList;
-    }
-    return dataList
+      return dataList
   },
 
 }
+
+exports.util = util;
